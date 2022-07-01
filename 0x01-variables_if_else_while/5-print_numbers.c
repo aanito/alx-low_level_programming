2ea20050c6063a85668132a072ee458e678f3e4e
root <howmarad@gmail.com>
2022-07-01 13:05:48 -0700

    Array of numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints base ten numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	{
+		printf("\n");
+	}
+
+	return (0);
+}
