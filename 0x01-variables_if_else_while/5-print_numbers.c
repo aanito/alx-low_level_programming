66b5a18d678940fe60f8de07d5f44114e31cfe0d
root <howmarad@gmail.com>
2022-07-01 13:49:43 -0700

    Print those songle digits of base ten

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-/**
- * main - prints base ten numbers
- *
- * Return: Always 0 (Success)
- */
 
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int a;
-	
-	for (a = 0; a < 10; a++)
-	{
-		printf("%d", a);
-	}
-	{
-		printf("\n");
-	}
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
 
 	return (0);
 }
