87dcc628162b94512447aff1b8bebb0940318dc3
root <howmarad@gmail.com>
2022-07-01 13:24:16 -0700

    Iterate with only putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-/*
- * main - Print all single sigit numbers using the putchar starting from 0,
- * 	only using putchar 
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char num;
+	int num;
 
 	for (num = 0; num < 10; num++)
-	{
 		putchar((num % 10) + '0');
-	}
-	{
-		putchar('\n');
-	}
+
+	putchar('\n');
 
 	return (0);
 }
