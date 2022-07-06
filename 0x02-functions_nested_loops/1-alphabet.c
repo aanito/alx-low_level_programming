326a3aebac6535c294f8dfae956aa2d29b1346da
root <howmarad@gmail.com>
2022-07-06 06:57:21 -0700

    Small alphabet with print small function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_small - priint small letters
+ *
+ * Retrun: 0 (Success)
+ */
+
+void print_small(void)
+{
+int a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
