c9abea63d8c1d15d7869a628507d85eb9d1d777d
root <howmarad@gmail.com>
2022-07-07 13:19:06 -0700

    Have the numbers printed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * main - prints the digits
+ * Return 0 (Success)
+ */
+
+void print_numbers(void);
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
