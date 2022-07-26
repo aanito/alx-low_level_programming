0cc0b59298695f4c2984d128edfce2eb83f2d60c
root <howmarad@gmail.com>
2022-07-25 22:12:21 -0700

    First allocate memory

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *create_array - creates an array off chars
+ *@size: size of the array
+ *@c: the element
+ *Return -a pointer to the array or NULL if it fails
+ */
+
+ char *create_array(unsigned int size, char c)
+ {
+     char *a;
+     unsigned int i;
+
+     if (size == 0)
+        return (NULL);
+
+     a = malloc(size * sizeof(*a);
+     if (a == NULL)
+        return (NULL);
+
+     for (i = 0; i < size; i++)
+        a[i] = c;
+
+     return 0;
+ }
