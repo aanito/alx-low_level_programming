e1a9285e0c631680fa1e76867c8848d442a00d10
root <howmarad@gmail.com>
2022-07-25 22:23:13 -0700

    Hot fix

--- 0x08-malloc_free/0-create_array.c
+++ 0x08-malloc_free/0-create_array.c
@@ -3,23 +3,23 @@
  *create_array - creates an array off chars
  *@size: size of the array
  *@c: the element
- *Return -a pointer to the array or NULL if it fails
+ *Return: a pointer to the array or NULL if it fails
  */
 
- char *create_array(unsigned int size, char c)
- {
-     char *a;
-     unsigned int i;
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
 
-     if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-     a = malloc(size * sizeof(*a);
-     if (a == NULL)
-        return (NULL);
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
 
-     for (i = 0; i < size; i++)
-        a[i] = c;
+	for (i = 0; i < size; i++)
+		a[i] = c;
 
-     return 0;
- }
+	return (a);
+}
