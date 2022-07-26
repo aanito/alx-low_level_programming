72aa250ca9cad20e27a8d7e8e1cf8c1888c38f3f
root <howmarad@gmail.com>
2022-07-25 23:50:34 -0700

    Duplicate the string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_strdup - function returns a newly allocated space
+*@str: initial string
+*@nstr: new string
+*Return: a pointer
+*/
+char *_strdup(char *str)
+{
+    char *str;
+    char *nstr;
+
+    *nstr = strcpy(str, nstr);
+    nstr = char *malloc(unsigned int size, *nstr);
+    if (size == 0)
+        return (NULL);
+    return (*nstr);
+}
