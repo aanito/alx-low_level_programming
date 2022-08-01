95e4d77784d61b902cc6ff236678b46ad28ff9e5
root <howmarad@gmail.com>
2022-08-01 11:37:23 -0700

    Create a struct here

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
-#ifndef _dog_h
-#define _dog_h
-#define struct dog
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - such structure, wow
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
 #endif
