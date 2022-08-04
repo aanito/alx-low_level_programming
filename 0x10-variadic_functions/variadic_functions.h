40c6d495f40f79c72ef2544ebef4c2618569c306
root <howmarad@gmail.com>
2022-08-04 13:48:36 -0700

    Print the strings of variadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,5 +16,6 @@ typedef struct print
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
