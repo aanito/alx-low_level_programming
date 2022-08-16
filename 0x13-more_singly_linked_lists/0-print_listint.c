b12c6d002ad002b94d6ac56e1998789a8fff0044
root <howmarad@gmail.com>
2022-08-15 22:56:14 -0700

    Print the elements of the list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include "lists.h"
+
+/**
+ * print_listint - Prints int list-
+ * @h: pointer to main list
+ * Return: Numbers of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
