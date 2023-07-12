bb727eb977e7e0bf10c3d2d2c76e55e93fafeb56
aanito <howmarad@gmail.com>
2023-07-12 16:10:08 +0300

    Jump search in a singly linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -30,5 +30,12 @@ int _binary_search(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
+
 
 #endif
